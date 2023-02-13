5b53575b5a7d65fa9f2a3cc93e3a66c69462c465
GGG <GGG@DESKTOP-8N3GG4O>
2023-02-13 16:08:25 +0900

    test중...

---
+++ zerozerotwo/src/GasStation/test.java
@@ -0,0 +1,16 @@
+package GasStation;
+
+import java.sql.Connection;
+
+public class test {
+	private static GasStationDAO dao;
+	private Connection conn;
+	
+	public static void main(String[] args) {
+		dao = new GasStationDAOImpl();
+		
+		List<GasStation> 
+		
+		
+	}
+}
