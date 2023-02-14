235cda1889ef11623246b1f2932e8ac3e4ecf8a3
GGG <GGG@DESKTOP-8N3GG4O>
2023-02-14 15:53:04 +0900

    servlet

---
+++ zerozerotwo/src/userInfo/UserInfoServlet.java
@@ -0,0 +1,7 @@
+package userInfo;
+
+import javax.servlet.http.HttpServlet;
+
+public class UserInfoServlet extends HttpServlet {
+
+}
--- zerozerotwo/src/userInfo/userService.java
+++
@@ -1,5 +0,0 @@
-package userInfo;
-
-public interface userService {
-
-}
